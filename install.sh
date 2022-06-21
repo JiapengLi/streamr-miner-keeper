eb45dd2bfa7828f5e5b8fe7dd9ee604fa1a5e080
JiapengLi <mail@jiapeng.me>
2022-06-21 21:16:36 +0800

    update install.sh

--- install.sh
+++ install.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
 CONFFILE=$1
-USER=root
+USER=$2
 TARGET_NAME=streamr-miner-keeper
 WORKSPACE=/opt/streamr-miner-keeper
 
-mkdir -p $WORKSPACE
-cp streamr.py streamrlib.py $WORKSPACE
-cp $CONFFILE $WORKSPACE/conf.json
-
 PYTHON=`which python3`
 
+mkdir -p $WORKSPACE
+cp streamr.py streamrlib.py streamrdb.py $WORKSPACE
+cp $CONFFILE $WORKSPACE/conf.json
+
+cd $WORKSPACE
+chown $USER:$USER -R $WORKSPACE
+sudo -u $USER $PYTHON streamr.py cfg2db conf.json
 
 cat << EOF > /lib/systemd/system/$TARGET_NAME.service
 [Unit]
@@ -19,7 +22,7 @@ After=network.target
 
 [Service]
 WorkingDirectory=$WORKSPACE
-ExecStart=$PYTHON streamr.py keeper conf.json
+ExecStart=$PYTHON streamr.py keeper
 User=$USER
 
 [Install]
@@ -31,8 +34,8 @@ cat << EOF > /lib/systemd/system/$TARGET_NAME.timer
 Description=Streamr Miner Keeper timer
 
 [Timer]
-OnCalendar=*-*-* 01/2:20:00
-#OnCalendar=*-*-* *:0/30
+#OnCalendar=*-*-* 01/2:20:00
+OnCalendar=*-*-* *:20/30
 Unit=$TARGET_NAME.service
 
 [Install]
